c79e6130e0f9c7c25a3b26741400267a993ad1ec
sszth <2810688370@qq.com>
2020-10-23 16:54:28 +0800

    TCP socket支持

--- HXNetwork/HXWindowsSocketCommon.cpp
+++ HXNetwork/HXWindowsSocketCommon.cpp
@@ -7,10 +7,75 @@
 class HXWindowsSocket : public IHXSocket
 {
 public:
-
-	virtual bool Socket()
+	HXWindowsSocket() :m_socket(INVALID_SOCKET) {}
+public:
+	virtual bool Socket(HXSOCKET_AF af, HXSOCKET_TYPE type, HXSOCKET_PROTOCOL protocol) override
 	{
-		m_socket = ::socket();
+		m_socket = ::socket(HXSOCKET_AF_TO_WINAF(af), HXSOCKET_TYPE_TO_WINTYPE(type), HXSOCKET_PROTOCOL_TO_WINPROTOCOL(protocol));
+		if (INVALID_SOCKET == m_socket)
+		{
+			return false;
+		}
+		return true;
+	}
+
+	virtual bool Connect() override
+	{
+		::connect(m_socket,)
+	}
+
+	int HXSOCKET_AF_TO_WINAF(HXSOCKET_AF af)
+	{
+		int nWinAF = 0;
+		switch (af)
+		{
+		case HXSOCKET_AF_UNSPEC:
+			nWinAF = AF_UNSPEC;
+			break;
+		case HXSOCKET_AF_INET:
+			nWinAF = AF_INET;
+			break;
+		default:
+			break;
+		}
+		return nWinAF;
+	}
+
+	int HXSOCKET_TYPE_TO_WINTYPE(HXSOCKET_TYPE type)
+	{
+		int nWinType = 0;
+		switch (type)
+		{
+		case HXSOCKET_STREAM:
+			nWinType = SOCK_STREAM;
+			break;
+		default:
+			break;
+		}
+		return nWinType;
+	}
+
+	int HXSOCKET_PROTOCOL_TO_WINPROTOCOL(HXSOCKET_PROTOCOL protocol)
+	{
+		int nWinProtocol = 0;
+		switch (protocol)
+		{
+		case PROTOCOL_ICMP:
+			nWinProtocol = IPPROTO_ICMP;
+			break;
+		case PROTOCOL_IGMP:
+			nWinProtocol = IPPROTO_IGMP;
+			break;
+		case PROTOCOL_RFCOMM:
+			//nWinProtocol = BTHPROTO_RFCOMM;
+			break;
+		case PROTOCOL_TCP:
+			nWinProtocol = IPPROTO_TCP;
+			break;
+		default:
+			break;
+		}
+		return nWinProtocol;
 	}
 private:
 	SOCKET m_socket;
@@ -42,7 +107,7 @@ int HXWindowsSocketCommon::Initialize()
 IHXSocket_Ptr HXWindowsSocketCommon::GetSocket()
 {
 	IHXSocket_Ptr ptr = std::make_shared<HXWindowsSocket>();
-	::socket
+
 	return ptr;
 }
 
--- HXNetwork/IHXSocket.h
+++ HXNetwork/IHXSocket.h
@@ -1,12 +1,34 @@
 #pragma once
-//地址族。
+// 目前只支持tcp
+// 地址族。
 enum HXSOCKET_AF
 {
-	AF_UNSPEC = 0,	//未定义
-	AF_INET	= 1,	//IPV4
-
+	HXSOCKET_AF_UNSPEC	= 0,	//未定义
+	HXSOCKET_AF_INET	= 1,	//IPV4
+	//HXSOCKET_AF_IPX		= 2,	//
+	//AF_APPLETALK
+	//AF_NETBIOS
+	//AF_INET6
+	//AF_IRDA
+	//AF_BTH
 };
-
+// 类型
+enum HXSOCKET_TYPE
+{
+	HXSOCKET_STREAM = 0,//tcp
+	//SOCK_DGRAM
+	//SOCK_RAW
+	//SOCK_RDM
+	//SOCK_SEQPACKET
+};
+// 协议
+enum HXSOCKET_PROTOCOL
+{
+	PROTOCOL_ICMP = 0,
+	PROTOCOL_IGMP = 1,
+	PROTOCOL_RFCOMM = 2,
+	PROTOCOL_TCP = 3,
+};
 class IHXSocket
 {
 	HXNOCOPY(IHXSocket);
@@ -14,7 +36,8 @@ public:
 	IHXSocket();
 	~IHXSocket();
 
-	virtual bool Socket() = 0;
+	virtual bool Socket(HXSOCKET_AF af, HXSOCKET_TYPE type, HXSOCKET_PROTOCOL protocol) = 0;
+	virtual bool Connect() = 0;
 };
 HXDECLARE_PTR(IHXSocket);
 
